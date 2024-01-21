b21fe877c965ca70b704ce22332cd3fe1ebc175e
Angga diki Saputra <anggadikisaputraagung@gmail.com>
2024-01-21 16:29:11 +0700

    set up prisma and postgresql

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import "dotenv/config";
-
+import { PrismaClient } from "@prisma/client";
 const Express = require("express");
-const port = process.env.PORT;
 
+const prisma = new PrismaClient();
+const port = process.env.PORT;
 const app = Express();
 
 app.get("/", (req: Request, res: Response) => {
@@ -11,6 +12,11 @@ app.get("/", (req: Request, res: Response) => {
     { id: 1, name: "test" },
     { id: 2, name: "initial" },
   ]);
+});
+
+app.get("/product", async (req: Request, res: Response) => {
+  const products = await prisma.product.findMany();
+  res.json(products);
 });
 
 app.listen(port, () => {
